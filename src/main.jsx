953f80f42a4978fddb8a0c9cd7d0d75b13e3e837
yuliia volkova <yuliia_volkova@icloud.com>
2025-03-30 15:13:42 +0200

    added tasks with map method

--- src/main.jsx
+++ src/main.jsx
@@ -4,3 +4,21 @@ import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
+
+/*const nums = [1, 2, 3, 4, 5];
+const doubled = nums.map(function (num) {
+  return num * num;
+});
+console.log(doubled);
+
+const names = ["alice", "bob", "charlie", "david"];
+const uppercased = names.map((name) => {
+  return name[0].toUpperCase() + name.slice(1);
+});
+console.log(uppercased);
+
+const pokemon = ["Bulbasaur", "Charmander", "Squirtle"];
+const pokeList = pokemon.map((poke) => {
+  return `<p>${poke}</p>`;
+});
+console.log(pokeList); */
